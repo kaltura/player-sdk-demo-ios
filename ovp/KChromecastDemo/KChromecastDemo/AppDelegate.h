de978e9e4dd81b8e81ee509c6142f7c53ea684c6
Eliza Sapir <elizasapir@Elizas-MacBook-Pro.local>
2015-12-07 11:51:02 +0200

    new demo for chromecast plugin

---
+++ ovp/KChromecastDemo/KChromecastDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KChromecastDemo
+//
+//  Created by Eliza Sapir on 07/12/2015.
+//  Copyright © 2015 Kaltura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ovp/KChromecastDemo/KChromecastDemo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  KChromecastDemo
+//
+//  Created by Eliza Sapir on 07/12/2015.
+//  Copyright © 2015 Kaltura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <KALTURAPlayerSDK/KPViewController.h>
+
+@interface ViewController : UIViewController
+
+@property (retain, nonatomic) KPViewController *player;
+
+@end
+
